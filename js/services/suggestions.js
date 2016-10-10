843fdbad58bf330b2e88d78bd132186c18732b1a
Kyle <kyle.c.r.fahey@gmail.com>
2016-10-10 18:14:27 -0500

    Add Suggestion Add Comments Add likes / dislikes

--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -1,31 +1,27 @@
-app.factory('suggestions', [function(){
-  var demoSuggestions = {
-    posts: [
-    {
-      title: 'Free pizza at club meetings',
-      upvotes: 15,
-      downvotes: 12,
-      comments: [],
-    },
-
-      title: 'Make everybody vote republican'
-      upvotes: 15,
-      downvotes: 1,
-      comments: [],
-    },
-    {
-      title: 'Make everybody vote republican'
-      upvotes: 15,
-      downvotes: 1,
-      comments: [],
-    },
-    {
-      title: 'Kyle dances around like an idiot after eating pizza',
-      upvotes: 1,
-      downvotes:12,
-      comments: [],
-    },
-  ]
-};
-  return demoSuggestions;
+app.factory('suggestions', [function() {
+	var demoSuggestions = {
+		posts: [
+			{
+				title: 'Free pizza at club meetings',
+				upvotes: 15,
+				comments: []
+			},
+			{
+				title: 'End all club emails with Laffy Taffy jokes',
+				upvotes: 9,
+				comments: []
+			},
+			{
+				title: 'Retrofit water fountain with Gatorade',
+				upvotes: 7,
+				comments: []
+			},
+			{
+				title: 'Sing Bon Jovi\'s "Living on a Prayer" halfway through meetings',
+				upvotes: 3,
+				comments: []
+			}
+		]
+	};
+	return demoSuggestions;
 }]);
